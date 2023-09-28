5a2d95783ad0d5e17c22fc78d92c32ed6198eda5
rcaicedom <r.caicedom@uniandes.edu.co>
2023-09-28 00:29:58 -0500

    Add list and menu page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AlarmsListComponent } from './pages/alarms-list/alarms-list.component';
 import { AlarmsMenuComponent } from './pages/alarms-menu/alarms-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, AlarmsListComponent, AlarmsMenuComponent],
+  declarations: [AppComponent, AlarmsMenuComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/pages/alarms-menu/alarms-menu.component.ts
+++ src/app/pages/alarms-menu/alarms-menu.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlarmsMenuComponent  implements OnInit {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Inbox', url: '/alarms-menu/inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/alarms-menu/outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/alarms-menu/favorites', icon: 'heart' },
+    { title: 'Archived', url: '/alarms-menu/archived', icon: 'archive' },
+    { title: 'Trash', url: '/alarms-menu/trash', icon: 'trash' },
+    { title: 'Spam', url: '/alarms-menu/spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() { }
